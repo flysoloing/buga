31e3ef41d7d31f88c90d0c219ae25438fb123916
flysoloing <flysoloing@gmail.com>
2013-04-07 18:37:31 +0800

    update domain src

---
+++ code/buga/buga-domain/src/main/java/com/flysoloing/buga/domain/Lang.java
@@ -0,0 +1,11 @@
+package com.flysoloing.buga.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-4-7
+ * Time: 下午5:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lang {
+}
